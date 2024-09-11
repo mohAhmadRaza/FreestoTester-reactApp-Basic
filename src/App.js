685805e13e225876fdc90bb5694b048bcee43fed
mohAhmadRaza <sktfscm21557034@gmail.com>
2024-09-11 21:23:48 +0500

    Here, I write code to change the title dynamically and added react router to open my pages without loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import "./App.css";
 import About from "./components/About";
+import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [prompt, setPrompt] = useState('Enable Dark Mode');
-  
+  const [mode, setMode] = useState("light");
+  const [prompt, setPrompt] = useState("Enable Dark Mode");
+
   // Use States for Alert
   const [alert, setAlert] = useState(null);
-  
+
   const showAlert = (message, type) => {
     setAlert({
       message: message,
-      type: type
+      type: type,
     });
 
     setTimeout(() => {
       setAlert(null);
     }, 2000);
   };
-  
+
   // Changing background of accordian
   const [mystyle, setMyStyle] = useState({
     color: "white",
@@ -29,63 +31,76 @@ function App() {
   });
 
   const toggleMode = () => {
-    if (prompt.includes('Dark')) {
-      setMode('dark');
-      setPrompt('Enable Light Mode');
-      document.body.style.backgroundColor = 'black';
+    if (prompt.includes("Dark")) {
+      setMode("dark");
+      setPrompt("Enable Light Mode");
+      document.body.style.backgroundColor = "black";
       setMyStyle({
-        color: 'white',
-        backgroundColor: 'black'
+        color: "white",
+        backgroundColor: "black",
       });
       showAlert("Dark mode enabled", "success");
-    } else if (prompt.includes('Light')) {
-      setMode('light');
-      setPrompt('Enable Dark Mode');
-      document.body.style.backgroundColor = 'white';
+      document.title = "FreesToTester - Dark Mode Enabled";
+    } else if (prompt.includes("Light")) {
+      setMode("light");
+      setPrompt("Enable Dark Mode");
+      document.body.style.backgroundColor = "white";
       setMyStyle({
-        color: 'black',
-        backgroundColor: 'white'
+        color: "black",
+        backgroundColor: "white",
       });
       showAlert("Light mode enabled", "success");
+      document.title = "FreesToTester - Dark Mode Enabled";
     }
+    setTimeout(() => {
+      document.title = "FreesToTester - Home";
+    }, 1500);
   };
-  
+
   const GreenMode = () => {
-    document.body.style.backgroundColor = 'green'; // Use the newMode directly
+    document.body.style.backgroundColor = "green"; // Use the newMode directly
     setMyStyle({
-      color : 'green',
-      text : 'white'
-    })
+      color: "green",
+      text: "white",
+    });
   };
   const BlueMode = () => {
-    document.body.style.backgroundColor = 'blue'; // Use the newMode directly
+    document.body.style.backgroundColor = "blue"; // Use the newMode directly
     setMyStyle({
-      color : 'white',
-      backgroundColor: 'blue'
-    })
+      color: "white",
+      backgroundColor: "blue",
+    });
   };
   const YellowMode = () => {
-    document.body.style.backgroundColor = 'yellow'; // Use the newMode directly
+    document.body.style.backgroundColor = "yellow"; // Use the newMode directly
     setMyStyle({
-      color : 'black',
-      backgroundColor: 'yellow'
-    })
+      color: "black",
+      backgroundColor: "yellow",
+    });
   };
 
   return (
     <>
-      <Navbar 
-        title="FreesToTester" 
-        homeText="Home" 
-        mode={mode} 
-        toogleMode={toggleMode} 
-        prompt={prompt} 
-        GreenMode={GreenMode}
-        BlueMode={BlueMode}
-        YellowMode={YellowMode}
-      />
-      <Alert alert={alert} />
-      <About mystyle={mystyle} />
+      <Router>
+        <Navbar
+          title="FreesToTester"
+          aboutText="About"
+          homeText="Home"
+          mode={mode}
+          toogleMode={toggleMode}
+          prompt={prompt}
+          GreenMode={GreenMode}
+          BlueMode={BlueMode}
+          YellowMode={YellowMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+          <Route path="/About" element={<About mystyle={mystyle} />} />
+          <Route path="/Home" element={<TextForm Heading="FreestoTester"/>} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -25,45 +26,14 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link className="nav-link active" aria-current="page" to="/Home">
                   {props.homeText}
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/About">
                   {props.aboutText}
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
+                </Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link disabled" aria-disabled="true" href="/">
